5d6c8c78564c4b4d18380a5c329fcf31e77d6f88
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-03-12 11:55:09 +0100

    20230312

---
+++ my_app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DocumentumUploadComponent } from './documentum-upload/documentum-upload.component';
+import { ViewDocumentsComponent } from './view-documents/view-documents.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocumentumUploadComponent,
+    ViewDocumentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my_app/src/app/documentum-upload/documentum-upload.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentum-upload',
+  templateUrl: './documentum-upload.component.html',
+  styleUrls: ['./documentum-upload.component.css']
+})
+export class DocumentumUploadComponent {
+
+}
