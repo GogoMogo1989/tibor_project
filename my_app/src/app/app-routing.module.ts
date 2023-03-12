c47973e4f1f0a3d60c6c309c59819933113da050
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-03-12 12:31:54 +0100

    20230312

--- my_app/src/app/app-routing.module.ts
+++ my_app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentumUploadComponent } from './documentum-upload/documentum-upload.component';
+import { ViewDocumentsComponent } from './view-documents/view-documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'documentum-upload', component: DocumentumUploadComponent},
+  {path:'view-documents', component: ViewDocumentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
