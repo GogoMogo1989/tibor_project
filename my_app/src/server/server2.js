6f568bfa5dfb7caa1d4c29f4c5710ddc2e47712e
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-03-15 19:55:24 +0100

    20230315

--- my_app/src/server/server2.js
+++ my_app/src/server/server2.js
@@ -1,49 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
-app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.json({ limit: '500mb' }));
 
-const url = 'mongodb+srv://GogoMogo1989:Password12345@cluster0.v457sky.mongodb.net/?retryWrites=true&w=majority'; // A MongoDB Atlas kapcsolati URL-je
+// MongoDB kapcsolódás
+const url = 'mongodb+srv://GogoMogo1989:Password12345@cluster0.v457sky.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('A MongoDB adatbázishoz sikeresen kapcsolódva!');
+  })
+  .catch((err) => {
+    console.log('Hiba a MongoDB adatbázis kapcsolat során:', err);
+  });
 
+// Adat sémája
+const dataSchema = new mongoose.Schema({
+  file: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^data:[a-z]+\/[a-z]+;base64,/.test(v);
+      },
+      message: 'A fájl nem base64 kódolt.'
+    }
+  }
+});
+
+// Adat modellje
+const DataModel = mongoose.model('Data', dataSchema);
+
+// API végpontok
 app.get('/api', (req, res) => {
   res.send('Ez egy Node.js backend!');
 });
 
 app.post('/api/data', (req, res) => {
-  const data = req.body;
+  const data = new DataModel(req.body);
   console.log(data);
 
-  MongoClient.connect(url, function(err, client) { // Kapcsolódás a MongoDB adatbázishoz
-    if (err) throw err;
-    const db = client.db("Base64"); // Adatbázis kiválasztása
-    const collection = db.collection("Base64"); // Kollekció kiválasztása
-    collection.insertOne(data, function(err, result) { // Adatok beszúrása a kollekcióba
-      if (err) throw err;
-      console.log('Az adatok mentése sikeres volt!');
-      res.send('Adatok sikeresen fogadva és mentve a szerveren.');
-      client.close();
-    });
+  data.save().then(() => {
+    console.log('Az adatok mentése sikeres volt!');
+    res.send('Adatok sikeresen fogadva és mentve a szerveren.');
+  }).catch((err) => {
+    console.log('Hiba az adatok mentésekor:', err);
+    res.status(500).send('Hiba az adatok mentésekor!');
   });
 });
 
 app.get('/api/dataget', (req, res) => {
- 
-  MongoClient.connect(url, function(err, client) { // Kapcsolódás a MongoDB adatbázishoz
-    if (err) throw err;
-    const db = client.db("Base64"); // Adatbázis kiválasztása
-    const collection = db.collection("Base64"); // Kollekció kiválasztása
-    collection.find({}).toArray(function(err, data) { // Adatok lekérdezése a kollekcióból
-      if (err) throw err;
-      res.send(data);
-      client.close();
-    });
+  DataModel.find({}).then((data) => {
+    res.send(data);
+  }).catch((err) => {
+    console.log('Hiba az adatok lekérdezésekor:', err);
+    res.status(500).send('Hiba az adatok lekérdezésekor!');
   });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`A szerver fut a ${port}-es porton!`));
+// Szerver indítása
+app.listen(port, () => {
+  console.log(`A szerver fut a ${port}-es porton!`);
+});
