2e6d0256e3f04c09043ae103d419161d5daa6b40
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-03-28 11:10:44 +0200

    20230328

--- my_app/src/app/services/loginservices.ts
+++ my_app/src/app/services/loginservices.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, of } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
