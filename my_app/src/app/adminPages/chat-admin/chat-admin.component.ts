b8ea59354dd8804bc5cce642e0caa0fc6baf904b
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-04-25 13:16:05 +0200

    20230425

--- my_app/src/app/adminPages/chat-admin/chat-admin.component.ts
+++ my_app/src/app/adminPages/chat-admin/chat-admin.component.ts
@@ -24,12 +24,15 @@ export class ChatAdminComponent {
       });
   }
 
-  get messages(): ChatMessage[] {
+
+  //Ellenőrizzük, hogy az üzenetet a felhasználó küldte-e, vagy a felhasználó kapta-e, és ezálltal változik az email cím, és így a visszatérő üzenet listájához adódik hozzá
+  get messages(): ChatMessage[] { 
     return this.chatService.messages.filter(message => 
-      (message.email === this.selectedUser && message.recipientEmail === this.authService.getEmail()) ||
-      (message.email === this.authService.getEmail() && message.recipientEmail === this.selectedUser)
+      (message.email === this.selectedUser && message.recipientEmail === this.authService.getEmail())/*  kapott  */|| 
+      (message.email === this.authService.getEmail() && message.recipientEmail === this.selectedUser) /* küldött */
     );
   }
+
   sendAdminMessage() {
     this.chatService.sendMessage(this.adminMessage, this.selectedUser, this.authService.getEmail());
     this.adminMessage="";
